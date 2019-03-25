404c4376eca4cb228a236216e2f959ae1d95e183
Elvis Ibarra <ielvisd@gmail.com>
2019-03-25 16:00:17 -0700

    strings.c complete

--- strings/strings.c
+++ strings/strings.c
@@ -9,13 +9,13 @@
 */
 int string_length(char *s)
 {
-  printf("*s is: %c\n", *s); 
-  printf("*s is: %d\n", &s); 
+//   printf("*s is: %c\n", *s); 
+//   printf("*s is: %d\n", &s); 
   
   int c;
   
   for (c = 0; s[c] != '\0'; c++) {
-   printf("%c\n", s[c]);
+//    printf("%c\n", s[c]);
   }
   
   return (c-1);
@@ -29,7 +29,27 @@ int string_length(char *s)
 */
 char *reverse_string(char *rv, char *s)
 {
+  
+  
 
+  int stringLength = string_length(s);
+  
+  printf("length of array is: %d\n", stringLength);
+
+  int c;
+  //Set last elminate to a null character
+  rv[stringLength + 1] = '\0';
+  //loop through elements in quote starting at the end
+  for (c = stringLength; c >= 0; c--) {
+    printf("s[c] is: %c\n", s[c]);
+    printf("c is: %d\n", c);
+    // set first element to current element
+    rv[stringLength - c] = s[c];
+  }
+  // return reversed string
+  return rv;
+  
+  
 }
 
 #ifndef TESTING
@@ -38,7 +58,7 @@ int main(void)
     char quote1[] = "Don't forget to be awesome";
     char quote2[] = "a man a plan a canal panama";
 
-    char rv[512];
+    char rv[30];
 
     printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
     printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
@@ -46,4 +66,3 @@ int main(void)
     return 0;
 }
 #endif
-    s
